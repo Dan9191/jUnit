f85cadb90b450b8965b9f4a533b05d6a684a9f81
danis <-@>
2023-11-09 01:10:18 +0500

    MockitoExtension

--- src/test/java/com/dan/junit/service/UserServiceTest.java
+++ src/test/java/com/dan/junit/service/UserServiceTest.java
@@ -27,10 +27,17 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.exceptions.base.MockitoException;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -42,6 +49,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.lenient;
 
 @Tag("user")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -52,6 +60,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
         UserServiceParamResolver.class,
         PostProcessingExtension.class,
         ConditionalExtension.class,
+        MockitoExtension.class
      //   ThrowableExtension.class // blocking all exceptions
         // GlobalExtension.class             // we included custom extension, but now we extend TestBase
 })
@@ -59,8 +68,16 @@ public class UserServiceTest extends TestBase {
 
     private static final User PETR = User.of(2, "Petr", "111");
     private static final User IVAN = User.of(1, "Ivan", "123");
-    private UserService userService;
+
+    @Captor
+    private ArgumentCaptor<Integer> argumentCaptor;
+
+    //@Spy
+    @Mock(lenient = true)
     private UserDao userDao;
+
+    @InjectMocks
+    private UserService userService;
 
     UserServiceTest(TestInfo testInfo) {
         System.out.println();
@@ -82,14 +99,24 @@ public class UserServiceTest extends TestBase {
     @BeforeEach
     void prepare(UserService userService) {
         System.out.println("Before each:" + this);
-        this.userDao = Mockito.spy(UserDao.class);
-        this.userService = new UserService(userDao);
+        Mockito.doReturn(true).when(userDao).delete(IVAN.getId());
+   //     lenient().when(userDao.delete(IVAN.getId())).thenReturn(true);
+   //     Mockito.mock(UserDao.class, Mockito.withSettings().lenient());
+//        this.userDao = Mockito.spy(UserDao.class);
+//        this.userService = new UserService(userDao);
+    }
+
+
+    @Test
+    void throwExceptionIfDatabaseIsNotAvailable() {
+        Mockito.doThrow(RuntimeException.class).when(userDao).delete(IVAN.getId());
+        assertThrows(RuntimeException.class, () -> userService.delete(IVAN.getId()));
     }
 
     @Test
-    void shouldDeleteExistedUSer() {
+    void shouldDeleteExistedUser() {
         userService.add(IVAN);
-        Mockito.doReturn(true).when(userDao).delete(IVAN.getId()); // this is stub
+     //   Mockito.doReturn(true).when(userDao).delete(IVAN.getId()); // this is stub
         // Mockito.doReturn(true).when(userDao).delete(Mockito.any()); // this is dummy
 
        // Mockito.when(userDao.delete(IVAN.getId()))
@@ -100,7 +127,7 @@ public class UserServiceTest extends TestBase {
 
         boolean deleteResult = userService.delete(IVAN.getId());
         System.out.println(userService.delete(IVAN.getId()));
-        var argumentCaptor = ArgumentCaptor.forClass(Integer.class); // знвчение аргумента, с которым был вызван метод реального класса
+       // var argumentCaptor = ArgumentCaptor.forClass(Integer.class); // знвчение аргумента, с которым был вызван метод реального класса
         Mockito.verify(userDao, Mockito.atLeast(2)).delete(argumentCaptor.capture());  // checks the number of calls
 
 
@@ -112,9 +139,9 @@ public class UserServiceTest extends TestBase {
     @Test
     @Order(1)
     void usersEmptyIfNoUserAdded() throws IOException {
-        if (true) {
-            throw new RuntimeException("Custom exception for check throwable extension");
-        }
+//        if (true) {
+//            throw new RuntimeException("Custom exception for check throwable extension");
+//        }
         System.out.println("test1:" + this);
         List<User> users = userService.getAll();
         assertTrue(users.isEmpty());
